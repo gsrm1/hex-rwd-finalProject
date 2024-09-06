946505b12ed08ca75524a3f2dfdfe745a08d4052
gsrm1 <gsrm1@hotmail.com>
2024-09-06 10:54:04 +0800

    9/6 index fin

--- script/all.js
+++ script/all.js
@@ -1,3 +1,12 @@
 $(document).ready(function () {
-    
-});
+    // menu效果
+    $('.menu-hamburgerIcon').on('click', function (e) {
+      e.preventDefault();
+      $('.nav').toggleClass('menu-show');
+    });
+
+    $('.menu li').click(function (e) { 
+        e.preventDefault();
+        $(this).addClass('active').siblings().removeClass('active');
+    });
+  });
