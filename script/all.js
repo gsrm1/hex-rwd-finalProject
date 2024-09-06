693c3302402656dae7e5a0bc002589a681682feb
gsrm1 <gsrm1@hotmail.com>
2024-09-06 13:23:41 +0800

    9/6 gototop icon

--- script/all.js
+++ script/all.js
@@ -9,4 +9,33 @@ $(document).ready(function () {
         e.preventDefault();
         $(this).addClass('active').siblings().removeClass('active');
     });
+
+    // goToTop icon 使用捲動百分比觸發按鈕
+    function showBtnCondition(){
+    const scrollTop = $(this).scrollTop();
+    const docHeight = $(document).height();
+    const windowHeight = $(window).height();
+    const scrollPercent = (scrollTop / (docHeight - windowHeight)) * 100;
+    if (scrollPercent > 50){
+      $('#goToTop').fadeIn();
+    }
+    else{
+      $('#goToTop').fadeOut();
+    }
+  }
+    $(window).scroll(showBtnCondition);
+
+    $('#goToTop').click(function (e) { 
+      $('html,body').animate({scrollTop: 0}, 800);
+    });
+
+    // goToTop icon 使用.css改變樣式
+    $('#goToTop').on('mouseover', function () {
+      $('.fa-circle-up').css('color', 'red');
+    });
+    
+    $('#goToTop').on('mouseleave', function () {
+      $('.fa-circle-up').css('color', 'orange');
+    });
+
   });
